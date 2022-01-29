44ce94b6a92a221e1ffdaed683b797753d911680
migueltorres040 <migueltorres040@gmail.com>
2022-01-28 23:22:32 -0300

    Update index.js

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,4 +5,4 @@ mongoose.connect('mongodb://localhost:27017/project-javascript')
         .then(()=>{
             console.log("conexion establecida");
         })
-        .catch(err=>console.log(err))
+        .catch(err=>console.log(err));
