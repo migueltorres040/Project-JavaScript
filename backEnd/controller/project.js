034ab69d0d7bcba5fafb1543ffcaa0cb0aab5b0f
migueltorres040 <migueltorres040@gmail.com>
2022-01-29 17:03:18 -0300

    Controladores
    Se crean los controladores y el método guardar

---
+++ backEnd/controller/project.js
@@ -0,0 +1,22 @@
+'use strict'
+var Project=require('../models/project');
+var fs=require('fs');//libreria de nodeJs file system , es para borrar un archivo
+
+//clase que contiene los metodos
+var controller={
+    //metodo guardar
+    saveProject:function(req,res){
+        var project=new Project(); //instancio el objeto para acceder a los objetos del modelo
+        var params=req.body;
+        project.user=params.user;
+        project.email=params.email;
+        project.key=params.key;
+        project.save((err,projectStored)=>{
+            if(err) return res.status(500).send({message:"error al guardar el documento"});
+            if(!projectStored) return res.status(404).send({message:"no se pudo guardar el documento"});
+            return res.status(200).status({project,projectStored});
+        });
+    }
+};
+
+module.exports=controller;
