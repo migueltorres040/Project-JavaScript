d5ffcd0181f07c7dc057229dfde11b99d731c69c
migueltorres040 <migueltorres040@gmail.com>
2022-01-28 23:22:03 -0300

    Dependencias - conexión base datos
    Se instalan las dependencias y se realiza la conexión a la base de datos

---
+++ backEnd/index.js
@@ -0,0 +1,8 @@
+'use strict'
+var mongoose=require('mongoose');
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://localhost:27017/project-javascript')
+        .then(()=>{
+            console.log("conexion establecida");
+        })
+        .catch(err=>console.log(err))
